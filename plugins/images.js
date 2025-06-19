dd41c596cff35d3b52c7ca0531a57de09d9c7c26
AhmedT1995 <60567838+AhmedT1995@users.noreply.github.com>
2025-06-19 21:48:31 +0200

    Update images.js

--- plugins/images.js
+++ plugins/images.js
@@ -1,17 +1,4 @@
 import axios from 'axios';
-
-let cooldown = new Set();
-
-let handler = async (m, { conn }) => {
-  if (cooldown.has(m.sender)) {
-    return await conn.sendMessage(m.chat, { text: '⏳ يرجى الانتظار ثانيتين قبل استخدام هذا الأمر مرة أخرى.' }, { quoted: m });
-  }
-
-  cooldown.add(m.sender);
-  setTimeout(() => {
-    cooldown.delete(m.sender);
-  }, 2000); 
-
   try {
     let res = (await axios.get(`https://raw.githubusercontent.com/Seiyra/imagesfjsfasfa/refs/heads/main/okay.js`)).data;
 
