1efb627e6a2b7707fe356b860e7b2accf87ed9ed
AhmedT1995 <60567838+AhmedT1995@users.noreply.github.com>
2025-08-31 17:49:52 +0200

    Update unban.js

--- plugins/unban.js
+++ plugins/unban.js
@@ -1,11 +1,23 @@
-const handler = async (m) => {
-  global.db.data.chats[m.chat].isBanned = false;
-  m.reply("The chat has been unbanned."); 
-};
+let handler = async (m, { conn }) => {
+    // التحقق إذا الشات موجود في قاعدة البيانات
+    if (!(m.chat in global.DATABASE._data.chats)) 
+        return m.reply('*هذا الشات غير مسجل في قاعدة البيانات!*');
+    
+    let chat = global.DATABASE._data.chats[m.chat];
+
+    // التحقق إذا الشات غير محظور أصلاً
+    if (!chat.isBanned) 
+        return m.reply('*هذا الشات غير محظور أصلاً!*');
+
+    // رفع الحظر
+    chat.isBanned = false;
+    m.reply('*✅ تم رفع الحظر عن هذا الشات!*');
+}
 
 handler.help = ['unbanchat'];
-handler.tags = ['owner'];
+handler.tags = ['عام'];
 handler.command = /^unbanchat$/i;
-handler.rowner = true;
+handler.owner = false;
+handler.admin = true;
 
-export default handler
+module.exports = handler;
