25660c6181c785edc354c4e164392e3cacbd1fc8
AhmedT1995 <60567838+AhmedT1995@users.noreply.github.com>
2025-08-31 17:23:23 +0200

    Update unban.js

--- plugins/unban.js
+++ plugins/unban.js
@@ -1,11 +1,15 @@
+import fs from 'fs';
+
 const handler = async (m) => {
+  // إلغاء الحظر عن الشات
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply("The chat has been unbanned."); 
+  m.reply("✅ The chat has been unbanned."); // رسالة بعد فك الحظر
 };
 
+// معلومات الأمر
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
-handler.command = /^unbanchat$/i;
-handler.rowner = true;
+handler.command = /^unbanchat$/i; 
+handler.rowner = true; // بس الـ owner يقدر يستخدمه
 
 export default handler;
